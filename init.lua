7e283ef5d41d222e0cc4a3b34e14dec28bf214ab
Ron Sanzone <ron.sanzone1@gmail.com>
2025-06-23 00:15:41 -0700

    Add vim-be-better

--- init.lua
+++ init.lua
@@ -989,6 +989,13 @@ require('lazy').setup({
       vim.g['test#strategy'] = 'vimux'
     end,
   },
+  {
+    'szymonwilczek/vim-be-better',
+    config = function()
+      -- Optional: Enable logging for debugging
+      vim.g.vim_be_better_log_file = 1
+    end,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
